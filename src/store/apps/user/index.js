0761e63b63bccbf7dd32939b04d36ba75c3efe9a
kalememre <kalememre@icloud.com>
2023-12-20 11:37:50 +0000

    user error handler

--- src/store/apps/user/index.js
+++ src/store/apps/user/index.js
@@ -9,51 +9,79 @@ import axiosInstance from 'src/store/axiosDefaults'
 
 // ** Get all Users
 export const getUsers = createAsyncThunk('appUser/getUsers', async () => {
-    const response = await axiosInstance.get('/user/')
+    try {
+        const response = await axiosInstance.get('/user/')
 
-    return response.data
+        return response.data
+    } catch (error) {
+        throw error.response.data
+    }
 })
 
 // ** Get User by ID
 export const getUserById = createAsyncThunk('appUser/getUserById', async (id) => {
-    const response = await axiosInstance.get(`/user/${id}/`)
+    try {
+        const response = await axiosInstance.get(`/user/${id}/`)
 
-    return response.data
+        return response.data
+    } catch (error) {
+        throw error.response.data
+    }
 })
 
 // ** Add new User
 export const addUser = createAsyncThunk('appUser/addUser', async (user) => {
-    const response = await axiosInstance.post('/user/', user)
+    try {
+        const response = await axiosInstance.post('/user/', user)
 
-    return response.data
+        return response.data
+    } catch (error) {
+        throw error.response.data
+    }
 })
 
 // ** Suspended User
 export const suspendUser = createAsyncThunk('appUser/suspendUser', async (id) => {
-    const response = await axiosInstance.put(`/user/suspend/${id}`)
+    try {
+        const response = await axiosInstance.put(`/user/suspend/${id}`)
 
-    return response.data
+        return response.data
+    } catch (error) {
+        throw error.response.data
+    }
 })
 
 // ** Update User
 export const updateUser = createAsyncThunk('appUser/updateUser', async (user) => {
-    const response = await axiosInstance.put(`/user/${user.id}/`, user)
+    try {
+        const response = await axiosInstance.put(`/user/${user.id}/`, user)
 
-    return response.data
+        return response.data
+    } catch (error) {
+        throw error.response.data
+    }
 })
 
 // ** Update User Profile
 export const updateUserProfile = createAsyncThunk('appUser/updateUserProfile', async (user) => {
-    const response = await axiosInstance.put(`/user/profile/${user.id}/`, user)
+    try {
+        const response = await axiosInstance.put(`/user/profile/${user.id}/`, user)
 
-    return response.data
+        return response.data
+    } catch (error) {
+        throw error.response.data
+    }
 })
 
 // ** Update User Branches
 export const updateUserBranches = createAsyncThunk('appUser/updateUserBranches', async (data) => {
-    const response = await axiosInstance.put(`/user/branches/${data.id}/`, data.branches)
+    try {
+        const response = await axiosInstance.put(`/user/branches/${data.id}/`, data.branches)
 
-    return response.data
+        return response.data
+    } catch (error) {
+        throw error.response.data
+    }
 })
 
 
@@ -74,14 +102,11 @@ export const appUserSlice = createSlice({
         })
         builder.addCase(getUsers.fulfilled, (state, action) => {
             state.users = action.payload
-
-            // toast.success('Users loaded successfully')
             state.userLoading = false
         })
         builder.addCase(getUsers.rejected, (state, action) => {
             state.error = action.payload
-
-            // toast.error('Something went wrong')
+            toast.error(`Something went wrong\n\n${action.error.message}`)
             state.userLoading = false
         })
         builder.addCase(addUser.pending, (state, action) => {
@@ -89,14 +114,12 @@ export const appUserSlice = createSlice({
         })
         builder.addCase(addUser.fulfilled, (state, action) => {
             state.users.push(action.payload)
-
-            // toast.success('User added successfully')
+            toast.success('User added successfully')
             state.userLoading = false
         })
         builder.addCase(addUser.rejected, (state, action) => {
             state.error = action.payload
-
-            // toast.error('Something went wrong')
+            toast.error(`Something went wrong\n\n${action.error.message}`)
             state.userLoading = false
         })
         builder.addCase(getUserById.pending, (state, action) => {
@@ -104,14 +127,11 @@ export const appUserSlice = createSlice({
         })
         builder.addCase(getUserById.fulfilled, (state, action) => {
             state.user = action.payload
-
-            // toast.success('User loaded successfully')
             state.userLoading = false
         })
         builder.addCase(getUserById.rejected, (state, action) => {
             state.error = action.payload
-
-            // toast.error('Something went wrong')
+            toast.error(`Something went wrong\n\n${action.error.message}`)
             state.userLoading = false
         })
         builder.addCase(suspendUser.pending, (state, action) => {
@@ -123,7 +143,6 @@ export const appUserSlice = createSlice({
             const index = state.users.findIndex(user => user.id === action.payload.id)
             state.users[index] = action.payload
             state.user = action.payload
-
             toast.success('User suspended successfully')
             state.userLoading = false
             state.userProfileLoading = false
@@ -131,8 +150,7 @@ export const appUserSlice = createSlice({
         })
         builder.addCase(suspendUser.rejected, (state, action) => {
             state.error = action.payload
-
-            toast.error('Something went wrong')
+            toast.error(`Something went wrong\n\n${action.error.message}`)
             state.userLoading = false
             state.userProfileLoading = false
             state.userBranchesLoading = false
@@ -149,7 +167,7 @@ export const appUserSlice = createSlice({
         })
         builder.addCase(updateUser.rejected, (state, action) => {
             state.error = action.payload
-            toast.error('Something went wrong')
+            toast.error(`Something went wrong\n\n${action.error.message}`)
             state.userLoading = false
         })
         builder.addCase(updateUserProfile.pending, (state, action) => {
@@ -164,7 +182,7 @@ export const appUserSlice = createSlice({
         })
         builder.addCase(updateUserProfile.rejected, (state, action) => {
             state.error = action.payload
-            toast.error('Something went wrong')
+            toast.error(`Something went wrong\n\n${action.error.message}`)
             state.userProfileLoading = false
         })
         builder.addCase(updateUserBranches.pending, (state, action) => {
@@ -179,7 +197,7 @@ export const appUserSlice = createSlice({
         })
         builder.addCase(updateUserBranches.rejected, (state, action) => {
             state.error = action.payload
-            toast.error('Something went wrong')
+            toast.error(`Something went wrong\n\n${action.error.message}`)
             state.userBranchesLoading = false
         })
     }
